9ddbf3fc9564aea9ed8801ffc673021f4b44ff32
Homero Villanueva <homero.codervi@gmail.com>
2022-11-18 19:03:22 -0600

    Removing the comments by default

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Project initial. Working in the portfolio.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CODERVI</h1>
       </header>
     </div>
   );
